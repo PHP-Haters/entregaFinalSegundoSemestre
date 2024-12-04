9d6f47c2d054c93637fad1cf09adab31f90ab3bf
Aquamixi <debastianierick@gmail.com>
2024-12-04 19:45:11 -0300

    ajustes

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ async function loggin(url) {
 async function changeConfig() {
     var PersonId = 6;
     
-    const response = fetch(https://personal-ga2xwx9j.outsystemscloud.com/TaskBoard_CS/rest/TaskBoard/ConfigPersonTheme?PersonId=${PersonId}, {
+    const response = fetch(`https://personal-ga2xwx9j.outsystemscloud.com/TaskBoard_CS/rest/TaskBoard/ConfigPersonTheme?PersonId=${PersonId}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
