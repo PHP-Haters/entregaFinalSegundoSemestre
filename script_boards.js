61c2d2310f17a2b8861ff317fc0909139f27afe4
Aquamixi <debastianierick@gmail.com>
2024-12-04 21:00:01 -0300

    ajustes

--- script_boards.js
+++ script_boards.js
@@ -6,18 +6,20 @@ document.addEventListener("DOMContentLoaded", function() {
 async function fetchData() {
     try {
         const response = await fetch('https://personal-ga2xwx9j.outsystemscloud.com/TaskBoard_CS/rest/TaskBoard/Boards'); // Substitua com a URL da API
-        const tasks = await response.json();
+        const items = await response.json();
 
-        const taskList = document.getElementById('task-list');
-        tasks.forEach(task => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${task.Id}</td>
-                <td>${task.Name}</td>
-                <td>${task.Description}</td>
-                <td>${task.CreatedBy}</td>
+        const cardsContainer = document.getElementById('cards-container');
+        items.forEach(item => {
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.id = `${item.Id}`;
+            card.innerHTML = `
+                <h3>${item.Name}</h3>
             `;
-            taskList.appendChild(row);
+            cardsContainer.appendChild(card);
+            document.getElementById(`${item.Id}`).addEventListener('click', function(){
+                window.location.href = 'colunas.html?columnId='+item.Id;
+            })
         });
     } catch (error) {
         console.error('Erro ao buscar dados:', error);
