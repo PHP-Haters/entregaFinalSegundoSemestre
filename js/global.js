9ef601aebcc2863838fcd2b8f28d6bbf1cb83f81
gabriel-castro <gabrielcastro.developer@gmail.com>
2024-12-05 17:21:33 -0300

    logout function

--- js/global.js
+++ js/global.js
@@ -1,5 +1,16 @@
+// Pegar o nome do usuário e colocar no header
 const userNameSection = document.querySelector('#username-section');
 const loggedUser = JSON.parse(localStorage.getItem("usuario_logado"));
 
 userNameSection.innerText = loggedUser.Name;
 
+//Permitir que o usuário faça logout
+const logoutBtn = document.querySelector('#loggout-btn');
+
+
+location.href
+
+logoutBtn.addEventListener('click', () => {
+    localStorage.removeItem('usuario_logado');
+    location.href = '/index.html'
+});
