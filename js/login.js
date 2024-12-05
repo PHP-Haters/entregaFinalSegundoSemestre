b12d50acedb6f87b0b11a8960a149d0405f172af
gabriel-castro <gabrielcastro.developer@gmail.com>
2024-12-05 15:30:28 -0300

    new file structure

--- js/login.js
+++ js/login.js
@@ -1,13 +1,8 @@
-
 const toggleThemeButton = document.getElementById('toggle-theme');
-
 const showLoginButton = document.getElementById('show-login');
 const errorMessage = document.getElementById('error-message');
 const loginForm = document.getElementById('login-form');
 const sendLogin = document.getElementById('btn-login');
-
-
-
 
 function toggleForms(showForm, hideForm) {
     hideForm.classList.remove('active');
@@ -23,14 +18,14 @@ sendLogin.addEventListener('click', async function getUser(){
     var email = document.getElementById('login-email').value;
 
     var response = await loggin('https://personal-ga2xwx9j.outsystemscloud.com/TaskBoard_CS/rest/TaskBoard/GetPersonByEmail?Email='+email);
-    
+
     errorMessage.textContent = response.Errors[0];
 });
 
 async function loggin(url) {
     try {
         var response = await fetch(url);
-        
+
         if (response.status == 200) {
             var user = await response.json();
 
