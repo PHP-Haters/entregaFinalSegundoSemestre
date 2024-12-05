ad0bfd4a64633a7c120512f91c0297f355121f1e
Gabriel Castro <gabrielcastro.developer@gmail.com>
2024-12-05 19:34:04 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,24 +1,18 @@
 const errorMessage = document.getElementById('error-message');
 const loginForm = document.getElementById('login-form');
 const sendLogin = document.getElementById('btn-login');
+const form = document.querySelector('#login-form');
 
-function toggleForms(showForm, hideForm) {
-    hideForm.classList.remove('active');
-    showForm.classList.add('active');
-}
+form.addEventListener('submit', sendFormData);
+sendLogin.addEventListener('click', sendFormData);
 
-function setActiveButton(activeButton, inactiveButton) {
-    activeButton.classList.add('active');
-    inactiveButton.classList.remove('active');
-}
-
-sendLogin.addEventListener('click', async function getUser(){
+async function sendFormData(event){
     var email = document.getElementById('login-email').value;
 
     var response = await loggin('https://personal-ga2xwx9j.outsystemscloud.com/TaskBoard_CS/rest/TaskBoard/GetPersonByEmail?Email='+email);
 
     errorMessage.textContent = 'Erro: ' + response.Errors[0];
-});
+}
 
 async function loggin(url) {
     try {
