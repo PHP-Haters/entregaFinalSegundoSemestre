557fb5ca4f08f9fe8268c56f40af613279d22932
Gabriel Castro <gabrielcastro.developer@gmail.com>
2024-12-06 09:46:12 -0300

    boards update screen

--- js/boards.js
+++ js/boards.js
@@ -13,8 +13,30 @@ async function getBoards() {
     }
 }
 
+async function deleteBoard(id) {
+    try {
+        const response = await fetch(api + 'Board?BoardId=' + id, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            console.log(`Board ${id} deletado com sucesso.`);
+            
+            getBoards(); //Pega os boards novamente e atualiza a tela
+        } else {
+            console.error(`Erro ao deletar o board ${id}:`, response.statusText);
+            alert(`Erro ao deletar o board: ${response.statusText}`);
+        }
+    } catch (error) {
+        console.error('Erro na requisição DELETE:', error);
+        alert('Ocorreu um erro ao deletar o board. Tente novamente mais tarde.');
+    }
+}
+
 function updateScreen(boards){
     const cardsContainer = document.getElementById('cards-container');
+    cardsContainer.innerHTML = ''; //Limpa a tela
+
     boards.forEach(board => {
         const card = document.createElement('div');
         card.className = 'card';
@@ -23,7 +45,26 @@ function updateScreen(boards){
             <h3>${board.Name}</h3>
             <hr />
             <p>${board.Description}</p>
+            <div class="board-btns">
+                
+            </div>
         `;
+
+        // Adicionando o botão "Deletar"
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete-btn';
+        deleteButton.textContent = 'Deletar';
+
+        // Associando a função deleteBoard ao botão
+        deleteButton.addEventListener('click', function (event) {
+            event.stopPropagation(); // Evita que o clique afete o evento do card
+            deleteBoard(board.Id);
+        });
+
+        // Adiciona o botão dentro do div.board-btns
+        card.querySelector('.board-btns').appendChild(deleteButton);
+
+
         cardsContainer.appendChild(card);
         document.getElementById(`${board.Id}`).addEventListener('click', function(){
             localStorage.removeItem('boardId');
